ff03d016f4dac6385c7476624ae99f948bd3ce87
Alex Firsov <firdim0@gmail.com>
2021-06-01 09:04:39 +0300

    Initial Commit

---
+++ Palindrome/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Palindrome
+//
+//  Created by Grifus on 01.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
